a5791e42b3e9294423c13a25e6247e3b02e5d4b8
Jan Willem <janwillembuist@gmail.com>
2020-09-23 16:03:25 +0200

    Added a image that can be showed

--- scripts/simplescript.js
+++ scripts/simplescript.js
@@ -1,3 +1,10 @@
 function helloWorld() {
     alert('Hello world!');
 }
+
+function tend() {
+  document.getElementById('theImage').style.visibility='visible';
+}
+function tn() {
+  document.getElementById('theImage').style.visibility='hidden';
+}
